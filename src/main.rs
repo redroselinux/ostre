732df02cc15fa1bc793e21ce065db05594f456e9
iamnotmega <iamnotmega@proton.me>
2025-11-12 18:33:03 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,67 @@
+use std::{process::{Stdio, Command, Child}, io::{stdin, stdout, Write}, path::Path, env};
+
+fn main(){
+    loop {
+        print!("> ");
+        let _ = stdout().flush();
+
+        let mut input = String::new();
+        stdin().read_line(&mut input).unwrap();
+
+        let mut commands = input.trim().split(" .p ").peekable();
+        let mut previous_command = None;
+
+        while let Some(command) = commands.next()  {
+
+            let mut parts = command.trim().split_whitespace();
+            let command = parts.next().unwrap();
+            let args = parts;
+
+            match command {
+                "cd" => {
+                    let new_dir = args.peekable().peek()
+                        .map_or("/", |x| *x);
+                    let root = Path::new(new_dir);
+                    if let Err(e) = env::set_current_dir(&root) {
+                        eprintln!("{}", e);
+                    }
+
+                    previous_command = None;
+                },
+                "exit" => return,
+                command => {
+                    let stdin = previous_command
+                        .map_or(
+                            Stdio::inherit(),
+                            |output: Child| Stdio::from(output.stdout.unwrap())
+                        );
+
+                    let stdout = if commands.peek().is_some() {
+                        Stdio::piped()
+                    } else {
+                        Stdio::inherit()
+                    };
+
+                    let output = Command::new(command)
+                        .args(args)
+                        .stdin(stdin)
+                        .stdout(stdout)
+                        .spawn();
+
+                    match output {
+                        Ok(output) => { previous_command = Some(output); },
+                        Err(e) => {
+                            previous_command = None;
+                            eprintln!("{}", e);
+                        },
+                    };
+                }
+            }
+        }
+
+        if let Some(mut final_command) = previous_command {
+            let _ = final_command.wait();
+        }
+
+    }
+}
